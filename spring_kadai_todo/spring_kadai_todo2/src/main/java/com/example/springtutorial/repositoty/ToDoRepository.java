d1ab4f368936b137d63dc9905755378d483521fc
srn0209 <seri-0209@ezweb.ne.jp>
2025-09-09 12:57:48 +0900

    Delete spring_kadai_todo/spring_kadai_todo2 directory

--- spring_kadai_todo/spring_kadai_todo2/src/main/java/com/example/springtutorial/repositoty/ToDoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springtutorial.repositoty;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToDoRepository extends JpaRepository<com.example.springtutorial.entity.ToDo, Integer> {
-}
